8e5f52140d2a35df568a00bca10d53d233b84a56
nscomponents18 <nscomponents18@gmail.com>
2021-11-02 23:15:17 -0400

    Git Commit with Home Page details

---
+++ precache-manifest.b8498929ef7dbc3b68eb3a97c46a65d4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad2465ecb74b5febaeda36fa25ba394a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e583490a9236e5ca8306",
+    "url": "./static/css/2.95665f4a.chunk.css"
+  },
+  {
+    "revision": "d725c75ade2ee583af93",
+    "url": "./static/css/main.20953379.chunk.css"
+  },
+  {
+    "revision": "e583490a9236e5ca8306",
+    "url": "./static/js/2.3115c826.chunk.js"
+  },
+  {
+    "revision": "038137378776d3782021381009b8dd7f",
+    "url": "./static/js/2.3115c826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d725c75ade2ee583af93",
+    "url": "./static/js/main.2d243c60.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.2d243c60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a46056f6d6e7f590745",
+    "url": "./static/js/runtime-main.e3aa39cb.js"
+  },
+  {
+    "revision": "45c73723862c6fc5eb3d6961db2d71fb",
+    "url": "./static/media/fontawesome-webfont.45c73723.eot"
+  },
+  {
+    "revision": "4b5a84aaf1c9485e060c503a0ff8cadb",
+    "url": "./static/media/fontawesome-webfont.4b5a84aa.woff2"
+  },
+  {
+    "revision": "76a4f23c6be74fd309e0d0fd2c27a5de",
+    "url": "./static/media/fontawesome-webfont.76a4f23c.svg"
+  },
+  {
+    "revision": "7c87870ab40d63cfb8870c1f183f9939",
+    "url": "./static/media/fontawesome-webfont.7c87870a.ttf"
+  },
+  {
+    "revision": "dfb02f8f6d0cedc009ee5887cc68f1f3",
+    "url": "./static/media/fontawesome-webfont.dfb02f8f.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
